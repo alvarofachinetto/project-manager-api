f61024c8714c7d842e5400084b27fe6065059fb1
Alvaro <alvaro.facneto@gmail.com>
2025-03-03 12:16:02 -0300

    fase 1 projeto gerenciador de tarefa

---
+++ src/gateways/controllers/controllers.module.ts
@@ -0,0 +1,4 @@
+
+import { ProjectsController } from './projects/projects.controller';
+import { TasksController } from './tasks/tasks.controller';
+import { UsersController } from './users/users.controller';
